51b2e7380c74d920a4cb181a5636212951f2b188
CHINTHALA DHEERAJ <145866132+misterdheeru@users.noreply.github.com>
2025-01-12 09:42:24 +0530

    Added Design And Added Mat Date Picker Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'AngualrDatePicker', loadChildren: () => import('./angualr-date-picker/angualr-date-picker.module').then(m => m.AngualrDatePickerModuleEExample) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { AngualrDatePickerModuleEExample } from './angualr-date-picker/angualr-date-picker.module';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatInputModule,
+    AngualrDatePickerModuleEExample
   ],
   providers: [
     MatFormFieldModule,
