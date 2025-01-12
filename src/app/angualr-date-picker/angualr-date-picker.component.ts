0393bbeec48417ea27a9920a039ea09a7edbaf44
VENKAIAH CHINTHALA <c.venkaiah1972@gmail.com>
2025-01-12 12:02:33 +0530

    Added Date Filter

--- src/app/angualr-date-picker/angualr-date-picker.component.ts
+++ src/app/angualr-date-picker/angualr-date-picker.component.ts
@@ -4,22 +4,42 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-angualr-date-picker',
   standalone: false,
-  
+
   templateUrl: './angualr-date-picker.component.html',
   styleUrl: './angualr-date-picker.component.scss'
 })
 export class AngualrDatePickerComponent {
-  angularMatDatePicker!:FormGroup
-  constructor(private formbuilder :FormBuilder)
-  {
-  this.angularMatDatePicker= formbuilder.group({
-     formDate:[""],
-     toDate:[""]
+  minDate: any
+  maxDate: any
+  angularMatDatePicker!: FormGroup
+  constructor(private formbuilder: FormBuilder) {
+    this.angularMatDatePicker = formbuilder.group({
+      formDate: [""],
+      toDate: [""]
     })
   }
- 
-  clear()
-  {
-   this.angularMatDatePicker.reset()
+  fromDate = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+
+    this.minDate = new Date()
+
+    return day !== 0 && day !== 6;
+
+
+
+  };
+  toDate = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+
+
+    return day !== 0 && day !== 6;
+
+
+
+  };
+  clear() {
+    this.angularMatDatePicker.reset()
   }
 }
