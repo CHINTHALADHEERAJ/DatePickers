e172223baa0ef58066dd7c9a53605ab8eb719490
CHINTHALA DHEERAJ <145866132+misterdheeru@users.noreply.github.com>
2025-01-16 15:31:03 +0530

    Added Buttons

--- src/app/angular-ng-bootstrap-date-picker/angular-ng-bootstrap-date-picker.component.ts
+++ src/app/angular-ng-bootstrap-date-picker/angular-ng-bootstrap-date-picker.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component,inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+ 
 @Component({
   selector: 'app-angular-ng-bootstrap-date-picker',
   standalone: false,
@@ -8,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './angular-ng-bootstrap-date-picker.component.scss'
 })
 export class AngularNgBootstrapDatePickerComponent {
-
+  ngDatePickerFrom! : FormGroup
+  today = inject(NgbCalendar).getToday();
+  model: NgbDateStruct | null = null;
+  
+  displayMonths = 1;
+	navigation = 'select';
+	showWeekNumbers = false;
+	outsideDays = 'hidden';
+  placement="top";
+ constructor(private fb:FormBuilder)
+ {
+  this.ngDatePickerFrom =  fb.group({
+     datePicker:[""]
+   })
+ }
+ clear()
+ {
+   this.ngDatePickerFrom.reset();
+ }
 }
--- src/app/angular-ng-bootstrap-date-picker/angular-ng-bootstrap-date-picker.module.ts
+++ src/app/angular-ng-bootstrap-date-picker/angular-ng-bootstrap-date-picker.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 import { AngularNgBootstrapDatePickerRoutingModule } from './angular-ng-bootstrap-date-picker-routing.module';
 import { AngularNgBootstrapDatePickerComponent } from './angular-ng-bootstrap-date-picker.component';
 
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  NgbCalendar,
+  NgbDatepickerModule,
+  NgbDateStruct,
+} from '@ng-bootstrap/ng-bootstrap';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    AngularNgBootstrapDatePickerComponent
-  ],
+  declarations: [AngularNgBootstrapDatePickerComponent],
   imports: [
     CommonModule,
-    AngularNgBootstrapDatePickerRoutingModule
+    AngularNgBootstrapDatePickerRoutingModule,
+    NgbDatepickerModule,
+    FormsModule,
+    JsonPipe,
+    FormsModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
-  exports:[AngularNgBootstrapDatePickerComponent]
+  exports: [AngularNgBootstrapDatePickerComponent],
 })
-export class AngularNgBootstrapDatePickerModule { }
+export class AngularNgBootstrapDatePickerModule {}
